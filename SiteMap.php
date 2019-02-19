a53688127b2b93b75ce2832a43be14ec9aef0d0c
Ekaterina Boeva <katrinb_08@mail.ru>
2019-02-20 00:20:40 +0300

    added lemmatization, little fixes

--- SiteMap.php
+++ SiteMap.php
@@ -7,12 +7,25 @@ class SiteMap
     public $ignore_list;
     public $limit;
 
-    public function __construct($host, $ignore_list)
+    public function __construct($host, $ignore_list = null)
     {
         $this->host = $host;
         $this->limit = 100;
-        $this->ignore_list = $ignore_list;
-        $this->host_paths = $this->filter($this->findPages($this->host));
+        $this->host_paths = [];
+        if (!$ignore_list) {
+            $this->ignore_list = ["javascript:", ".css", ".js", ".ico", ".jpg", ".png", ".jpeg", ".swf", ".gif", '#', '@'];
+        } else {
+            $this->ignore_list = $ignore_list;
+        }
+    }
+
+    public function findPaths()
+    {
+        if (file_exists('data/index.txt') && $content = file_get_contents('data/index.txt')) {
+            $this->host_paths = explode("\n", $content);
+        } else {
+            $this->host_paths = $this->filter($this->findPages($this->host));
+        }
     }
 
     private function getRespCode($url)
@@ -23,7 +36,7 @@ class SiteMap
     private function findPages($page)
     {
         $content = file_get_contents($page);
-        preg_match_all("/<a[^>]*href\s*=\s*'([^']*)'|".'<a[^>]*href\s*=\s*"([^"]*)"'."/is", $content, $match);
+        preg_match_all("/<a[^>]*href\s*=\s*'([^']*)'|" . '<a[^>]*href\s*=\s*"([^"]*)"' . "/is", $content, $match);
 
         return $match[2];
     }
@@ -43,7 +56,8 @@ class SiteMap
         return $urls;
     }
 
-    private function validate($url){
+    private function validate($url)
+    {
         $valid = true;
 
         if (strpos($url, substr($this->host, 8)) === false || strpos($url, ' ') !== false
@@ -62,29 +76,62 @@ class SiteMap
         return $valid;
     }
 
-    public function updateListFile()
+    public function updateFiles()
     {
-        $content = '';
-        foreach ($this->host_paths as $path) {
-            $content .= $path . "\n";
-        }
+        file_put_contents('data/index.txt', implode("\n", $this->host_paths));
 
-        file_put_contents('index.txt', $content);
-    }
-
-    public function updateFilesContent()
-    {
         for ($i = 0; $i < $this->limit; $i++) {
-            file_put_contents('data/'.($i + 1).'.txt', strip_tags($this->getContent($this->host_paths[$i])));
+            file_put_contents('data/pages/' . ($i + 1) . '.txt', strip_tags($this->getContent($this->host_paths[$i])));
         }
     }
 
-    public function getContent($url)
+    private function getContent($url)
     {
-        $content = preg_replace("'<script[^>]*?>.*?</script>'si","", file_get_contents($url));
-        $content = preg_replace("'<style[^>]*?>.*?</style>'si","", $content);
+        $content = preg_replace("'<script[^>]*?>.*?</script>'si", "", file_get_contents($url));
+        $content = preg_replace("'<style[^>]*?>.*?</style>'si", "", $content);
         $content = str_replace("\n", " ", $content);
-        return preg_replace("/(\s){2,}/u"," ",$content);
+        return preg_replace("/(\s){2,}/u", " ", $content);
     }
 
+    private function getWords($filepath)
+    {
+        if (preg_match_all("/\b(\w+)\b/ui", file_get_contents($filepath), $matches)) {
+            foreach ($matches[1] as $key => $word) {
+                $matches[1][$key] = strtoupper(iconv('utf-8', 'windows-1251//IGNORE', $word));
+            }
+
+            return $matches[1];
+        }
+
+        return [];
+    }
+
+    private function saveLemmatizedFile($words, $filepath)
+    {
+        $str = implode("\t", $words);
+        if (strpos($str, "\t") === 0) {
+            $str = substr($str, 1);
+        }
+        if (strrpos($str, "\t") === strlen($str) - 1) {
+            $str = substr($str, 0, strlen($str) - 1);
+        }
+        while (strpos($str, "\t\t") !== false) {
+            $str = str_replace("\t\t", "\t", $str);
+        }
+
+        file_put_contents($filepath, strtolower($str));
+    }
+
+    public function lemmatizeFiles(phpMorphy $morphy)
+    {
+        for ($i = 1; $i <= $this->limit; $i++) {
+            $filename = $i .'.txt';
+            $words = $this->getWords('data/pages/'.$filename);
+            foreach ($words as $key => $word) {
+                $words[$key] = $morphy->lemmatize($word)[0];
+            }
+
+            $this->saveLemmatizedFile($words, 'data/lemmatized/'.$filename);
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 
+require_once 'phpmorphy-0.3.7/src/common.php';
 include 'SiteMap.php';
 
+$dir = 'phpmorphy-0.3.7/dicts';
+$lang = 'ru_RU';
+$opts = ['storage' => PHPMORPHY_STORAGE_FILE];
+
+try {
+    $morphy = new phpMorphy($dir, $lang, $opts);
+} catch (phpMorphy_Exception $e) {
+    die('Error occured while creating phpMorphy instance: ' . $e->getMessage());
+}
+
 $url = 'https://kpfu.ru';
-$ignoreList = ["javascript:", ".css", ".js", ".ico", ".jpg", ".png", ".jpeg", ".swf", ".gif", '#', '@'];
-$map = new SiteMap($url, $ignoreList);
-$map->updateListFile();
-$map->updateFilesContent();
+$map = new SiteMap($url);
+$map->findPaths();
+$map->updateFiles();
+$map->lemmatizeFiles($morphy);
